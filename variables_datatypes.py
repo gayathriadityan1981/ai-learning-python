59f4c985861cc3ab9349dea69796f0cadb24cca0
Gayathri <32363990+gayathriadityan1981@users.noreply.github.com>
2026-09-20 06:35:47 +0530

    Add files via upload
    Variables & Data Types

---
+++ variables_datatypes.py
@@ -0,0 +1,13 @@
+name = "Gayathri"
+age = 25
+is_student = True
+height = 5.3
+print(name)
+print(age)
+print(is_student)
+print(height)
+
+print(type(name))
+print(type(age))
+print(type(is_student))
+print(type(height))
