75c47c16e74e173f67bb3bf669c13f06364a57fd
Gayathri <32363990+gayathriadityan1981@users.noreply.github.com>
2026-09-20 06:27:16 +0530

    Add files via upload
    Practise print statements

---
+++ print_practice.py
@@ -0,0 +1,24 @@
+# Using print statement
+
+# Basic printing
+print("Python is fun!")
+
+# Printing multiple values
+name = "Gayathri"
+city = "Newyork"
+print("Name:", name, "| City:", city)
+
+# Using f-strings
+print(f"My name is {name} and I live in {city} ")
+
+print(f"My name is {name}")
+
+# Customizing separators and end character
+print("Python", "Java", "C++", sep=" | ", end=" <End>\n")
+
+print("Python", "Java", "C++", sep=" , ", end=" <I am at end>\n")
+
+print("Helo\n world")
+print("Name \t: Gayathri")
+print("He said \"hi\"")
+print("Displaying backslash\\")
